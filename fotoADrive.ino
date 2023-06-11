62456f76799ebb08fff34701dae9cc0e53226d85
Daniel Santos <santos.daniel88@uabc.edu.mx>
2023-06-10 22:02:23 -0700

    elimine lo que no queria

--- fotoADrive.ino
+++ fotoADrive.ino
@@ -66,7 +66,7 @@ const int Interval = 20000; //--> Capture and Send a photo every 20 seconds.
 // Variable to set capture photo with LED Flash.
 // Set to "false", then the Flash LED will not light up when capturing a photo.
 // Set to "true", then the Flash LED lights up when capturing a photo.
-bool LED_Flash_ON = true;
+// bool LED_Flash_ON = true;
 
 // Initialize WiFiClientSecure.
 WiFiClientSecure client;
@@ -247,144 +247,12 @@ void SendCapturedPhotos() {
 
 //________________________________________________________________________________ VOID SETUP()
 void setup() {
-  // put your setup code here, to run once:
-  
-  // Disable brownout detector.
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
-  
-  Serial.begin(115200);
-  Serial.println();
-  delay(1000);
-
-  pinMode(FLASH_LED_PIN, OUTPUT);
-  
-  // Setting the ESP32 WiFi to station mode.
-  Serial.println();
-  Serial.println("Setting the ESP32 WiFi to station mode.");
-  WiFi.mode(WIFI_STA);
-
-  //---------------------------------------- The process of connecting ESP32 CAM with WiFi Hotspot / WiFi Router.
-  Serial.println();
-  Serial.print("Connecting to : ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  
-  // The process timeout of connecting ESP32 CAM with WiFi Hotspot / WiFi Router is 20 seconds.
-  // If within 20 seconds the ESP32 CAM has not been successfully connected to WiFi, the ESP32 CAM will restart.
-  // I made this condition because on my ESP32-CAM, there are times when it seems like it can't connect to WiFi, so it needs to be restarted to be able to connect to WiFi.
-  int connecting_process_timed_out = 20; //--> 20 = 20 seconds.
-  connecting_process_timed_out = connecting_process_timed_out * 2;
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    digitalWrite(FLASH_LED_PIN, HIGH);
-    delay(250);
-    digitalWrite(FLASH_LED_PIN, LOW);
-    delay(250);
-    if(connecting_process_timed_out > 0) connecting_process_timed_out--;
-    if(connecting_process_timed_out == 0) {
-      Serial.println();
-      Serial.print("Failed to connect to ");
-      Serial.println(ssid);
-      Serial.println("Restarting the ESP32 CAM.");
-      delay(1000);
-      ESP.restart();
-    }
-  }
-
-  digitalWrite(FLASH_LED_PIN, LOW);
-  
-  Serial.println();
-  Serial.print("Successfully connected to ");
-  Serial.println(ssid);
-  //Serial.print("ESP32-CAM IP Address: ");
-  //Serial.println(WiFi.localIP());
-  //---------------------------------------- 
-
-  //---------------------------------------- Set the camera ESP32 CAM.
-  Serial.println();
-  Serial.println("Set the camera ESP32 CAM...");
-  
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-  
-  // init with high specs to pre-allocate larger buffers
-  if(psramFound()){
-    config.frame_size = FRAMESIZE_UXGA;
-    config.jpeg_quality = 10;  //0-63 lower number means higher quality
-    config.fb_count = 2;
-  } else {
-    config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 8;  //0-63 lower number means higher quality
-    config.fb_count = 1;
-  }
-  
-  // camera init
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    Serial.println();
-    Serial.println("Restarting the ESP32 CAM.");
-    delay(1000);
-    ESP.restart();
-  }
-
-  sensor_t * s = esp_camera_sensor_get();
-
-  // Selectable camera resolution details :
-  // -UXGA   = 1600 x 1200 pixels
-  // -SXGA   = 1280 x 1024 pixels
-  // -XGA    = 1024 x 768  pixels
-  // -SVGA   = 800 x 600   pixels
-  // -VGA    = 640 x 480   pixels
-  // -CIF    = 352 x 288   pixels
-  // -QVGA   = 320 x 240   pixels
-  // -HQVGA  = 240 x 160   pixels
-  // -QQVGA  = 160 x 120   pixels
-  s->set_framesize(s, FRAMESIZE_SVGA);  //--> UXGA|SXGA|XGA|SVGA|VGA|CIF|QVGA|HQVGA|QQVGA
-
-  Serial.println("Setting the camera successfully.");
-  Serial.println();
-
-  delay(1000);
-
-  Test_Con();
-
-  Serial.println();
-  Serial.println("ESP32-CAM captures and sends photos to the server every 20 seconds.");
-  Serial.println();
-  delay(2000);
+   
 }
 //________________________________________________________________________________ 
 
 //________________________________________________________________________________ VOID LOOP()
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  //---------------------------------------- Timer/Millis to capture and send photos to Google Drive every 20 seconds (see Interval variable).
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= Interval) {
-    previousMillis = currentMillis;
-
-    SendCapturedPhotos();
-  }
+ 
   //---------------------------------------- 
 }
